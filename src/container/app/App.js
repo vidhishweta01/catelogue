b89993d6099971fe8d3879d36ae9451eab47ca89
vidhishweta01 <vidhishweta01@gmail.com>
2021-09-08 17:26:56 +0530

    added route switch to app

--- src/container/app/App.js
+++ src/container/app/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Switch>
+        <Route path="/" component={} exact />
+        <Route path="/pokemon/:pokemon" component={} />
+      </Switch>
     </div>
   );
 }
