9710f5ec7cfeb5852be333ca5b5e2215ccac27a7
vidhishweta01 <vidhishweta01@gmail.com>
2021-09-08 19:51:11 +0530

    added url

--- src/component/pokelist/pokelist.js
+++ src/component/pokelist/pokelist.js
@@ -1,5 +1,33 @@
-const pokeList = () => (
-  <div>pokeList</div>
-);
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import FetchPokeData from '../../redux/action/pokeAction';
+
+const pokeList = () => {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state.PokeReducer);
+
+  useEffect(() => {
+    dispatch(FetchPokeData());
+  }, []);
+  console.log(state); // eslint-disable-line
+  console.log(state.items.length); // eslint-disable-line
+  const renderPokeList = () => {
+    if (state.loading) {
+      return <h1>loading...</h1>;
+    }
+
+    if (state.items.length > 0) {
+      const pokemon = state.items;
+      return pokemon.map((poke) => (
+        <p key={poke.url}>
+          {poke.name}
+          {' '}
+        </p>
+      ));
+    }
+    return <h1>cannot get pokemon list try again</h1>;
+  };
+  return (<div>{renderPokeList()}</div>);
+};
 
 export default pokeList;
