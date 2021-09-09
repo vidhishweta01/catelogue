eeaa0b231ac56c2fa65a2e824b61e32b19c173bd
vidhishweta01 <vidhishweta01@gmail.com>
2021-09-09 16:13:45 +0530

    make the info state array and each info is object

---
+++ src/component/Info/Info.js
@@ -0,0 +1,24 @@
+/* eslint-disable camelcase */
+import React from 'react';
+import propTypes from 'prop-types';
+
+const Info = ({ pokemon }) => {
+  const { name, height, base_experience } = pokemon;
+  return (
+    <div>
+      <h3>{name}</h3>
+      <p>{height}</p>
+      <p>{base_experience}</p>
+    </div>
+  );
+};
+
+Info.propTypes = {
+  pokemon: propTypes.shape({
+    name: propTypes.string.isRequired,
+    height: propTypes.string.isRequired,
+    base_experience: propTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default Info;
