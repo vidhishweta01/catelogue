8998109ebac8c0c561142e9dc6d20a5e9ef441fc
vidhishweta01 <vidhishweta01@gmail.com>
2021-09-15 18:20:17 +0530

    fix linter error

--- src/testing/snapshot/searchResult.test.js
+++
@@ -1,7 +0,0 @@
-import renderer from 'react-test-renderer';
-import SearchResult from '../../component/searchResult/searchResult';
-
-it('it matches the snapshot', () => {
-  const serchRes = renderer.create(SearchResult).toJSON();
-  expect(serchRes).toMatchSnapshot();
-});
--- src/testing/snapshot/searchSubmit.test.js
+++
@@ -1,7 +0,0 @@
-import renderer from 'react-test-renderer';
-import SearchForm from '../../component/search/searchSubmit';
-
-it('it matches the snapshot', () => {
-  const searchForm = renderer.create(SearchForm).toJSON();
-  expect(searchForm).toMatchSnapshot();
-});
